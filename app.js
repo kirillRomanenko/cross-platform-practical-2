2edc694c8e8c6c003da4560d3eb4579dcbaac8f1
Кирилл Романенко <romanenko.kirill1995@gmail.com>
2020-05-03 01:05:21 +0300

    create menu

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ const { app, ipcMain, BrowserWindow } = require('electron');
 
 function main() {
     let window = new BrowserWindow({
-        width: 288,
-        height: 520,
+        width: 500,
+        height: 500,
         webPreferences: {
             nodeIntegration: true
         }
     })
-    // window.webContents.openDevTools();
+    window.webContents.openDevTools();
     window.loadFile('./renderer/index.html');
+
+    let playWindow;
+    ipcMain.on('play-window', () => {
+        if (!playWindow) {
+            playWindow = new BrowserWindow({
+                width: 300,
+                height: 525,
+                parent: window,
+                webPreferences: {
+                    nodeIntegration: true
+                }
+            });
+            playWindow.loadFile('./renderer/play.html');
+            playWindow.on('closed', () => {
+                playWindow = null
+            })
+        }
+
+    })
 }
 app.on('ready', main)
---
+++ renderer/play.js
@@ -0,0 +1,85 @@
+const { ipcRenderer } = require('electron')
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
+
+let bird = new Image();
+let bg = new Image(); // Создание объекта
+let fg = new Image(); // Создание объекта
+let pipeUp = new Image(); // Создание объекта
+let pipeBottom = new Image(); // Создание объекта
+
+bird.src = "../img/flappy_bird_bird.png"; // Указание нужного изображения
+bg.src = "../img/flappy_bird_bg.png"; // Аналогично
+fg.src = "../img/flappy_bird_fg.png"; // Аналогично
+pipeUp.src = "../img/flappy_bird_pipeUp.png"; // Аналогично
+pipeBottom.src = "../img/flappy_bird_pipeBottom.png"; // Аналогично
+
+let gap = 90;
+
+// При нажатии на какую-либо кнопку
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+    yPos -= 25;
+    // fly.play();
+}
+
+// Создание блоков
+let pipe = [];
+
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+}
+
+let score = 0;
+// Позиция птички
+let xPos = 10;
+let yPos = 150;
+let grav = 1.5;
+
+function draw() {
+    ctx.drawImage(bg, 0, 0);
+
+    for (let i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+        pipe[i].x--;
+
+        if (pipe[i].x == 125) {
+            pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        }
+
+        // Отслеживание прикосновений
+        if (xPos + bird.width >= pipe[i].x
+            && xPos <= pipe[i].x + pipeUp.width
+            && (yPos <= pipe[i].y + pipeUp.height
+                || yPos + bird.height >= pipe[i].y + pipeUp.height + gap)) {
+
+            location.reload(); // Перезагрузка страницы
+            // ipcRenderer.send('reload-game');
+        }
+
+        if (pipe[i].x == 5) {
+            score++;
+            // score_audio.play();
+        }
+    }
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "24px Verdana";
+    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+
+    requestAnimationFrame(draw);
+}
+
+pipeBottom.onload = draw;
